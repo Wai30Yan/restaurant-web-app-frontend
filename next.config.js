d8d3c61c8f00e3f0d07a76cfe487a4b4530852c1
Wai Yan <mr.waiyanmyint@gmail.com>
2023-11-16 21:34:15 +1100

    copy the config from cna client project

--- next.config.js
+++ next.config.js
@@ -1,36 +1,34 @@
 /** @type {import('next').NextConfig} */
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-  
-module.exports = {
-  async rewrites() {
-    return [
-      // Proxy guest routes
-      {
-        source: '/guest/:path*',
-        destination: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com/guest/:path*',
-      },
-      // Proxy admin routes
-      {
-        source: '/admin/:path*',
-        destination: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com/admin/:path*',
-      },
-    ];
-  },
-  async serverMiddleware() {
-    // Set up the proxy for guest routes
-    this.middlewares.use('/guest', createProxyMiddleware({
-      target: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com',
-      changeOrigin: true,
-      pathRewrite: { '^/guest': '/guest' },
-    }));
-
-    // Set up the proxy for admin routes
-    this.middlewares.use('/admin', createProxyMiddleware({
-      target: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com',
-      changeOrigin: true,
-      pathRewrite: { '^/admin': '/admin' },
-    }));
-  },
+const nextConfig = {
   reactStrictMode: true,
 }
+
+module.exports = {
+  ...nextConfig,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_URL}/:path*`,
+      },
+    ]
+  },
+}
+
+
+// async serverMiddleware() {
+//   // Set up the proxy for guest routes
+//   this.middlewares.use('/guest', createProxyMiddleware({
+//     target: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com',
+//     changeOrigin: true,
+//     pathRewrite: { '^/guest': '/guest' },
+//   }));
+
+//   // Set up the proxy for admin routes
+//   this.middlewares.use('/admin', createProxyMiddleware({
+//     target: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com',
+//     changeOrigin: true,
+//     pathRewrite: { '^/admin': '/admin' },
+//   }));
+// },
