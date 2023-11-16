ea3fc0d24d87400a710693ee5a039e0524b0ab35
Wai Yan <mr.waiyanmyint@gmail.com>
2023-11-16 19:42:07 +1100

    configure rewrites routes to allow response from spring boot backend

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const rewrites = () => [
+  {
+    source: '/guest',
+    destination: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com/',
+  },
+  {
+    source: '/admin',
+    destination: 'http://web-app-backend-env.eba-fhgsij53.ap-southeast-2.elasticbeanstalk.com/',
+  },
+]
+
+
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: rewrites()
 }
 
 module.exports = nextConfig
